fa80e85aa1df8ccac5e2e96941c7494b6c6d9845
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2010-08-08 22:01:14 +0900

    fix poppler-grep

--- qt4/tests/poppler-grep.cpp
+++ qt4/tests/poppler-grep.cpp
@@ -1,5 +1,7 @@
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDebug>
+#include <QTextCodec>
+#include <iconv.h>
 
 #include <iostream>
 
@@ -9,7 +11,7 @@ int main( int argc, char **argv )
 {
     QCoreApplication a( argc, argv );               // QApplication required!
 
-    if (!( argc == 5 ))
+    if (!( argc == 5 )) /* XXX: more C++ or Qt4 manner */
     {
 	qWarning() << "usage: poppler-grep -q <n> <string> <filename>";
 	exit(1);
@@ -22,23 +24,43 @@ int main( int argc, char **argv )
 	exit(1);
     }
 
+    // QTextCodec::setCodecForCStrings(QTextCodec::codecForLocale());
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName( "EUC-JP" ));
+    // QTextCodec* codec = QTextCodec::codecForName( "EUC-JP" );
+    QString qstr_key = QString::fromLocal8Bit( argv[3] );
+    QByteArray qstr_utf8 = qstr_key.toUtf8();
+
+    std::cout << "*** given keyword: " << std::flush;
+    {
+      int j;
+      for ( j = 0; j < qstr_utf8.size(); j++ )
+        std::cout << qstr_utf8[j];
+      std::cout << std::endl;
+      std::cout << std::flush;
+    }
+
     int i;
     for ( i = 0; i < doc->numPages(); i++ )
     {
       int w, h;
-      int q = atoi( argv[2] );
+      int q = atoi( argv[2] ); /* XXX: more C++ manner */
       int x, y;
       int j = 0;
 
       Poppler::Page *page = doc->page(i);
-      w = doc->page(i)->pageSizeF().width();
-      h = doc->page(i)->pageSizeF().height();
+      w = page->pageSizeF().width();
+      h = page->pageSizeF().height();
       for ( x = 0; x < q ; x ++ ) 
       {
         for ( y = 0; y < q ; y ++ )
         {
           QRectF  rect = QRectF( x*w/q, y*h/q, (x+1)*w/q, (y+1)*h/q );
-          if ( page->search( argv[3], rect, Poppler::Page::FromTop, Poppler::Page::CaseInsensitive, Poppler::Page::Rotate0, true ) )
+          if ( page->search( qstr_key,
+                             rect,
+                             Poppler::Page::FromTop,
+                             Poppler::Page::CaseInsensitive,
+                             Poppler::Page::Rotate0,
+                             true ) )
           {
             std::cout << "*** Page " << i << ", section (" << x << "," << y << ")" << std::endl;
             std::cout << std::flush;
