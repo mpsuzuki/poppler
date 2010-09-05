59ac12203fbc78091e698369ec6049be6680391a
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2010-09-05 22:19:53 +0900

    all ASCII strings are localized

--- qt4/tests/poppler-texts.cpp
+++ qt4/tests/poppler-texts.cpp
@@ -10,11 +10,13 @@ int main( int argc, char **argv )
 {
     QCoreApplication a( argc, argv );               // QApplication required!
 
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForLocale());
+
     if ( argc < 2 ||
         (argc == 3 && strcmp(argv[2], "-raw") != 0 ) ||
          argc > 3)
     {
-	qWarning() << "usage: poppler-texts filename [-raw]";
+	qWarning() << QString::fromAscii( "usage: poppler-texts filename [-raw]" ).toLocal8Bit();
 	exit(1);
     }
   
@@ -25,18 +27,26 @@ int main( int argc, char **argv )
     Poppler::Document *doc = Poppler::Document::load(argv[1]);
     if (!doc)
     {
-	qWarning() << "doc not loaded";
+	qWarning() << QString::fromAscii( "doc not loaded" ).toLocal8Bit();
 	exit(1);
     }
-
-    QTextCodec::setCodecForCStrings(QTextCodec::codecForLocale());
 
     int i;
     for ( i = 0; i < doc->numPages(); i++ )
     {
-      int j = 0;
-      std::cout << "*** Page " << i << std::endl;
+      QByteArray mbcs;
+
+      mbcs = QString::fromAscii("*** Page ").toLocal8Bit();
       std::cout << std::flush;
+      for ( int j = 0; j < mbcs.size(); j++ )
+        std::cout << mbcs[j];
+      std::cout << std::endl;
+
+      mbcs = QString::number(i).toLocal8Bit();
+      std::cout << std::flush;
+      for ( int j = 0; j < mbcs.size(); j++ )
+        std::cout << mbcs[j];
+      std::cout << std::endl;
 
       Poppler::Page *page = doc->page(i);
       QRectF rect = QRectF( 0,
@@ -44,9 +54,9 @@ int main( int argc, char **argv )
                             (int)doc->page(i)->pageSizeF().width(),
                             (int)doc->page(i)->pageSizeF().height() );
 
-      QByteArray mbcs = page->text( rect, layout ).toLocal8Bit();
+      mbcs = page->text( rect, layout ).toLocal8Bit();
       std::cout << std::flush;
-      for ( j = 0; j < mbcs.size(); j++ )
+      for ( int j = 0; j < mbcs.size(); j++ )
         std::cout << mbcs[j];
       std::cout << std::endl;
 
