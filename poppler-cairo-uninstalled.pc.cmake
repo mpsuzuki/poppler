5d87bc8397b06b5222faf71b7d4aa2e8cf1b5f16
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2017-12-25 13:20:48 +0900

    [uninstalled.pc] cmake does not build intermediate libpoppler-cairo, libsplash

--- poppler-cairo-uninstalled.pc.cmake
+++ poppler-cairo-uninstalled.pc.cmake
@@ -3,4 +3,3 @@ Description: Cairo backend for Poppler PDF rendering library - uninstalled
 Version: @POPPLER_VERSION@
 Requires: poppler = @POPPLER_VERSION@ cairo >= @CAIRO_VERSION@
 
-Libs: -L${pc_top_builddir}/${pcfiledir}/poppler -lpoppler-cairo
--- poppler-splash-uninstalled.pc.cmake
+++ poppler-splash-uninstalled.pc.cmake
@@ -3,5 +3,3 @@ Description: Splash backend for Poppler PDF rendering library - uninstalled
 Version: @POPPLER_VERSION@
 Requires: poppler = @POPPLER_VERSION@
 
-Libs: -L${pc_top_builddir}/${pcfiledir}/splash -lsplash
-
