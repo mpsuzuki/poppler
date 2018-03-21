cdb823818e100d243d17620d568043ddc9638ab5
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2018-03-22 01:57:49 +0900

    improve Requires.private by Jeroen's suggestion

--- poppler.pc.cmake
+++ poppler.pc.cmake
@@ -5,6 +5,7 @@ includedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@
 Name: poppler
 Description: PDF rendering library
 Version: @POPPLER_VERSION@
+@PC_REQUIRES_POPPLER_PRIVATE@
 
 Libs: -L${libdir} -lpoppler
 Cflags: -I${includedir}/poppler
